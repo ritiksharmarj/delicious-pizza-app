593d72d754d484683ab486c5a3633ca4a1656e0e
ritiksharmarj <theonlyritik@gmail.com>
2023-03-01 20:44:45 +0530

    Init commit

---
+++ src/pages/About.js
@@ -0,0 +1,5 @@
+const About = () => {
+   return <h1>Ye About Page Hai Ree Baba...</h1>;
+};
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,5 @@
+const Home = () => {
+   return <h1>Ye Home Page Hai Ree Baba...</h1>;
+};
+
+export default Home;
